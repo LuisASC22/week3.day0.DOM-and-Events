78582dadde3dfcdcb5b13649835dd4a8e1dfa242
Luis Jimenez <lgj9706@gmail.com>
2022-08-02 15:16:54 -0500

    First Commit

---
+++ addingStyle/lib/js/main.js
@@ -0,0 +1,8 @@
+let rightSide = document.getElementsByClassName( "right_side")[0];
+
+rightSide.style.background="#d3c26f";
+
+let name1 = document.getElementsByClassName("name") [0];
+
+name1.style.textShadow = "-6px 5px 2px yellow";
+//
---
+++ theBrush/lib/js/main.js
@@ -0,0 +1,8 @@
+function alert1() {
+    alert("You've been deleted");
+    document.getElementById("rengar").src="assets/rengar.png";
+
+}
+
+
+document.addEventListener("click", function(){alert1()});
